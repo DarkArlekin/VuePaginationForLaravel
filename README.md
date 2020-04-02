e24c4d39d842fec94ca73a472b4084f7e5d5b98a
Yusif Zourab <wordlearningpress@gmail.com>
2020-04-02 16:13:08 +0300

    Releases #1

--- README.md
+++ README.md
@@ -23,21 +23,24 @@ npm install
 ```
 
 ### Import component
+```scss
+@import '~@ilegionxs/vue-pagination-for-laravel/dist/vue-pagination-for-laravel.css';
+```
 
 #### In Vue component
 The component name can be any valid.
 ```js
-import Pagination from 'vue-pagination-for-laravel';
+import Pagination from '@ilegionxs/vue-pagination-for-laravel';
 ```
 
 #### Global
 ```js
-Vue.component('vue-pagination', require('vue-pagination-for-laravel').default);
+Vue.component('vue-pagination', require('@ilegionxs/vue-pagination-for-laravel').default);
 ```
 
 ### Add tag with attributes
 The attribute "v-bind" should be valid [laravel pagination](https://laravel.com/docs/6.x/pagination).
-The attribute "update" should be valid get data.
+The attribute "update" should be valid method get data.
 ```vue
 <Pagination v-bind="users"
             @update="getData"></Pagination>
--- src/App.vue
+++ src/App.vue
@@ -102,7 +102,52 @@
 </script>
 
 <style lang="scss">
-    .page-link {
-        cursor: pointer;
+    nav {
+        display: block;
+
+        .pagination {
+            display: flex;
+            padding-left: 0;
+            list-style: none;
+            border-radius: .25rem;
+
+            .page-item {
+                cursor: pointer;
+
+                &:first-child {
+                    .page-link {
+                        margin-left: 0;
+                        border-top-left-radius: .25rem;
+                        border-bottom-left-radius: .25rem;
+                    }
+                }
+
+                &:last-child {
+                    .page-link {
+                        border-top-right-radius: .25rem;
+                        border-bottom-right-radius: .25rem;
+                    }
+                }
+
+                .page-link {
+                    &:hover {
+                        z-index: 2;
+                        color: #0056b3;
+                        text-decoration: none;
+                        background-color: #e9ecef;
+                        border-color: #dee2e6;
+                    }
+
+                    position: relative;
+                    display: block;
+                    padding: .5rem .75rem;
+                    margin-left: -1px;
+                    line-height: 1.25;
+                    color: #007bff;
+                    background-color: #fff;
+                    border: 1px solid #dee2e6;
+                }
+            }
+        }
     }
 </style>
